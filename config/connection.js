f89f5339413132300820dee406525e1fdddb5f18
Leo Galey <leogaley@gmail.com>
2018-01-09 17:41:26 -0600

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "NYUDuck",
-  database: "burgers_db"
-});
-
+if(process.env.JAWSDB_URL){
+	connection - mysql.createConnection(porcess.env.JAWSDB_URL);
+} else {
+		var connection = mysql.createConnection({
+		  host: "localhost",
+		  user: "root",
+		  password: "NYUDuck",
+		  database: "burgers_db"
+		});
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
